fb8a37bcc19ebff374605de6022a6639ed08dc9d
Kasra Karami <Kasra_K2K@yahoo.com>
2023-03-14 16:37:39 +0330

    create MongoDB pkg

---
+++ pgk/storage/mongo/mongo.go
@@ -0,0 +1,78 @@
+package mongo
+
+import (
+	"CEC/pgk/config"
+	"context"
+	"fmt"
+	"log"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+type Connection struct{}
+
+var Conn Connection
+
+func (c *Connection) Connect() *mongo.Client {
+	uri := config.AppConfig.MONGODB_URI
+
+	// Use the SetServerAPIOptions() method to set the Stable API version to 1
+	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
+	opts := options.Client().ApplyURI(uri).SetServerAPIOptions(serverAPI)
+
+	// Create a new client and connect to the server
+	client, err := mongo.Connect(context.TODO(), opts)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	return client
+}
+
+func (c *Connection) Disconnect(client *mongo.Client) {
+	defer func() {
+		if err := client.Disconnect(context.TODO()); err != nil {
+			log.Panic(err)
+		}
+	}()
+}
+
+func (c *Connection) Ping(client *mongo.Client) {
+	// Send a ping to confirm a successful connection
+	var result bson.M
+	if err := client.Database("admin").RunCommand(context.TODO(), bson.D{{Key: "ping", Value: 1}}).Decode(&result); err != nil {
+		panic(err)
+	}
+	fmt.Println("Pinged your deployment. You successfully connected to MongoDB!")
+}
+
+func (c *Connection) InsertOne(database, collection string, s interface{}) *mongo.InsertOneResult {
+	client := Conn.Connect()
+	defer Conn.Disconnect(client)
+
+	result, err := client.Database(database).Collection(collection).InsertOne(context.TODO(), s)
+	if err != nil {
+		message := fmt.Sprintf("error InsertOne database: `%s` collection: `%s`", database, collection)
+		log.Println(message, err)
+	}
+
+	return result
+}
+
+// func (c *Connection) FindOne(database, collection string, s interface{}) *mongo.SingleResult {
+// 	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+// 	defer cancel()
+
+// 	client := Conn.Connect()
+// 	defer Conn.Disconnect(client)
+
+// 	result, err := client.Database(database).Collection(collection).InsertOne(context.TODO(), s)
+// 	if err != nil {
+// 		message := fmt.Sprintf("error FindOne database: `%s` collection: `%s`", database, collection)
+// 		log.Println(message, err)
+// 	}
+
+// 	return result
+// }
