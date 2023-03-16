6c5d0839f673243347156977a1a7756b528f33bb
Kasra Karami <Kasra_K2K@yahoo.com>
2023-03-17 01:00:29 +0330

    move al pgk to pkg

--- pkg/helper/json.go
+++ pkg/helper/json.go
@@ -1,7 +1,7 @@
 package helper
 
 import (
-	"CEC/pgk/config"
+	"CEC/pkg/config"
 	"encoding/json"
 )
 
---
+++ pkg/storage/pg/pg.go
@@ -0,0 +1,50 @@
+package pg
+
+import (
+	"CEC/pkg/config"
+	"fmt"
+	"log"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+type connection struct {
+	DB *gorm.DB
+}
+
+var Conn connection
+
+/* --------------------------------- Connect -------------------------------- */
+// Connect to database and fill connection.DB
+/* -------------------------------------------------------------------------- */
+func (c *connection) Connect() {
+	appConfig := config.AppConfig
+
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s TimeZone=%s",
+		appConfig.DB_HOST,
+		appConfig.DB_USER,
+		appConfig.DB_PASSWORD,
+		appConfig.DB_NAME,
+		appConfig.DB_PORT,
+		appConfig.DB_SSL_MODE,
+		appConfig.DB_TIMEZONE,
+	)
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	c.DB = db
+}
+
+/* --------------------------------- Migrate -------------------------------- */
+// Create Table If Not Exist
+// Before use c.Migrate you have to run c.Connect() to fill c.DB
+/* -------------------------------------------------------------------------- */
+func (c *connection) Migrate(modelStruct interface{}) {
+	err := c.DB.AutoMigrate(&modelStruct)
+	if err != nil {
+		return
+	}
+}
