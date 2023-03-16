a8284b2a99e2c99e527c430302f6c4c2d565fb0f
Kasra Karami <Kasra_K2K@yahoo.com>
2023-03-17 01:01:19 +0330

    install fiber and add router

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,10 +1,27 @@
 package main
 
 import (
-	"CEC/pgk/config"
+	"CEC/pkg/config"
+	"CEC/pkg/storage/pg"
+	"fmt"
+	"log"
+
+	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
-	// Environment Variable
 	config.SetConfig()
+
+	app := fiber.New(fiber.Config{
+		Prefork:       false,
+		CaseSensitive: true,
+		StrictRouting: true,
+		ServerHeader:  "Fiber",
+		AppName:       "Compare Electric Car v1.0.0",
+	})
+
+	// Call to fill DB in struct
+	pg.Conn.Connect()
+
+	log.Fatal(app.Listen(fmt.Sprintf(":%s", config.AppConfig.PORT)))
 }
---
+++ cmd/routes/router.go
@@ -0,0 +1,21 @@
+package routes
+
+import (
+	"CEC/cmd/modules/user"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/monitor"
+)
+
+func Routes(app *fiber.App) {
+	app.Get("/_health", health)
+	app.Get("/_metrics", monitor.New(monitor.Config{Title: "Default Metrics Page"}))
+
+	api := app.Group("/api")
+	v1 := api.Group("/v1")
+	user.Routes(v1)
+}
+
+func health(c *fiber.Ctx) error {
+	return c.SendString("Everything works fine")
+}
