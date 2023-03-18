f83017ffef1406bc583a59a7c1fb1db4111507a9
Kasra Karami <Kasra_K2K@yahoo.com>
2023-03-18 11:17:19 +0330

    add new env

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -9,17 +9,20 @@ import (
 )
 
 type config struct {
-	PORT        string `json:"port"`
-	MODE        string `json:"mode"`
-	PREFORK     bool   `json:"prefork"`
-	DB_HOST     string `json:"db_host"`
-	DB_PORT     string `json:"db_port"`
-	DB_PASSWORD string `json:"dn_password"`
-	DB_USER     string `json:"db_user"`
-	DB_NAME     string `json:"db_name"`
-	DB_TIMEZONE string `json:"db_timezone"`
-	DB_SSL_MODE string `json:"db_ssl_mode"`
-	MONGODB_URI string `json:"mongodb_uri"`
+	PORT             string `json:"port"`
+	MODE             string `json:"mode"`
+	PREFORK          bool   `json:"prefork"`
+	BACKEND_VERSION  string `json:"backend_version"`
+	FRONTEND_VERSION string `json:"frontend_version"`
+	APP_VERSION      string `json:"app_version"`
+	DB_HOST          string `json:"db_host"`
+	DB_PORT          string `json:"db_port"`
+	DB_PASSWORD      string `json:"dn_password"`
+	DB_USER          string `json:"db_user"`
+	DB_NAME          string `json:"db_name"`
+	DB_TIMEZONE      string `json:"db_timezone"`
+	DB_SSL_MODE      string `json:"db_ssl_mode"`
+	MONGODB_URI      string `json:"mongodb_uri"`
 }
 
 var AppConfig config
@@ -39,6 +42,9 @@ func SetConfig() {
 	AppConfig.PORT = os.Getenv("PORT")
 	AppConfig.MODE = os.Getenv("MODE")
 	AppConfig.PREFORK = prefork
+	AppConfig.BACKEND_VERSION = os.Getenv("BACKEND_VERSION")
+	AppConfig.FRONTEND_VERSION = os.Getenv("FRONTEND_VERSION")
+	AppConfig.APP_VERSION = os.Getenv("APP_VERSION")
 	// PostgreSQL Database
 	AppConfig.DB_HOST = os.Getenv("DB_HOST")
 	AppConfig.DB_PORT = os.Getenv("DB_PORT")
