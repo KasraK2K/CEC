9521f752487a094d9d6a2747cdfc6e8757413494
Kasra Karami <Kasra_K2K@yahoo.com>
2023-03-19 13:39:47 +0330

    add some middleware

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,8 +3,18 @@ package main
 import (
 	"fmt"
 	"log"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cache"
+	"github.com/gofiber/fiber/v2/middleware/compress"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/etag"
+	"github.com/gofiber/fiber/v2/middleware/favicon"
+	"github.com/gofiber/fiber/v2/middleware/limiter"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/fiber/v2/middleware/recover"
+	"github.com/gofiber/fiber/v2/middleware/requestid"
 
 	"app/cmd/middleware"
 	"app/cmd/routes"
@@ -24,7 +34,16 @@ func main() {
 		AppName:       "Compare Electric Car v1.0.0",
 	})
 
-	// Middleware
+	// Fiber Middleware
+	app.Use(cache.New())
+	app.Use(compress.New())
+	app.Use(cors.New())
+	app.Use(etag.New())
+	app.Use(favicon.New())
+	app.Use(limiter.New(limiter.Config{Max: 100, Expiration: 60 * time.Second}))
+	app.Use(logger.New())
+	app.Use(recover.New())
+	app.Use(requestid.New())
 	app.Use(middleware.AddMiddleware)
 
 	// Router
