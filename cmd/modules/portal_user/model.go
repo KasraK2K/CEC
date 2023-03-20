d029be029ec9c4cf57b8053043b24dd25e869a5b
Kasra Karami <Kasra_K2K@yahoo.com>
2023-03-20 16:02:29 +0330

    add model filter and model filter validation on portal_user model

---
+++ cmd/modules/portal_user/model.go
@@ -0,0 +1,42 @@
+package portal_user
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+
+	"app/pkg/helper"
+)
+
+type PortalUser struct {
+	ID            uint           `json:"id"             bson:"id"             gorm:"type:uint;primaryKey;<-:false"`
+	Email         string         `json:"email"          bson:"email"          gorm:"type:string;unique;not null;"                        validate:"required,email,min=6,max=32"`
+	Password      string         `json:"password"       bson:"password"       gorm:"type:string;check:length(password) >= 8"             validate:"required,min=8,max=32"`
+	ContactNumber string         `json:"contact_number" bson:"contact_number" gorm:"type:string;unique;"`
+	FirstName     string         `json:"first_name"     bson:"first_name"     gorm:"type:string;"`
+	Surname       string         `json:"surname"        bson:"surname"        gorm:"type:string;"`
+	Gender        uint8          `json:"gender"         bson:"gender"         gorm:"type:int8;default:2"`
+	IsActive      bool           `json:"is_active"      bson:"is_active"      gorm:"default:true"`
+	IsAdmin       bool           `json:"is_admin"       bson:"is_admin"       gorm:"default:false"`
+	IsArchive     bool           `json:"is_archive"     bson:"is_archive"     gorm:"default:false"`
+	CreatedAt     time.Time      `json:"created_at"     bson:"created_at"     gorm:"type:timestamptz;autoCreateTime;"`
+	UpdatedAt     time.Time      `json:"updated_at"     bson:"updated_at"     gorm:"type:timestamptz;autoUpdateTime;"`
+	ArchiveAt     gorm.DeletedAt `json:"archive_at"     bson:"archive_at"     gorm:"type:timestamptz;index"`
+}
+
+func (portal_user *PortalUser) Validate() helper.ErrorResponse {
+	return helper.Validator(portal_user)
+}
+
+type PortalUserFilter struct {
+	ID        uint8  `json:"id"         bson:"id"`
+	Email     string `json:"email"      bson:"email"`
+	Gender    uint8  `json:"gender"     bson:"gender"`
+	IsActive  bool   `json:"is_active"  bson:"is_active"`
+	IsAdmin   bool   `json:"is_admin"   bson:"is_admin"`
+	IsArchive bool   `json:"is_archive" bson:"is_archive"`
+}
+
+func (filter *PortalUserFilter) Validate() helper.ErrorResponse {
+	return helper.Validator(filter)
+}
