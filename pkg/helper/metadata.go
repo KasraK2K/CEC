9627db672d4c4292bd056d1af9f1984fbe0e823d
Kasra Karami <Kasra_K2K@yahoo.com>
2023-03-20 14:05:51 +0330

    change any to interface in metadata helper

--- pkg/helper/metadata.go
+++ pkg/helper/metadata.go
@@ -3,16 +3,16 @@ package helper
 import "app/pkg/config"
 
 type metaData struct {
-	BACKEND_VERSION  string `json:"backend_version" bson:"backend_version"`
-	FRONTEND_VERSION string `json:"frontend_version" bson:"frontend_version"`
-	APP_VERSION      string `json:"app_version" bson:"app_version"`
-	MODE             string `json:"mode" bson:"mode"`
-	SUCCESS          bool   `json:"success" bson:"success"`
-	RESULT           any    `json:"result,omitempty" bson:"result,omitempty"`
-	ERRORS           any    `json:"errors" bson:"errors,omitempty"`
+	BACKEND_VERSION  string      `json:"backend_version" bson:"backend_version"`
+	FRONTEND_VERSION string      `json:"frontend_version" bson:"frontend_version"`
+	APP_VERSION      string      `json:"app_version" bson:"app_version"`
+	MODE             string      `json:"mode" bson:"mode"`
+	SUCCESS          bool        `json:"success" bson:"success"`
+	RESULT           interface{} `json:"result,omitempty" bson:"result,omitempty"`
+	ERRORS           interface{} `json:"errors" bson:"errors,omitempty"`
 }
 
-func AddMetaData(data any, errors ...bool) *metaData {
+func AddMetaData(data interface{}, errors ...bool) *metaData {
 	metadata := metaData{
 		BACKEND_VERSION:  config.AppConfig.BACKEND_VERSION,
 		FRONTEND_VERSION: config.AppConfig.FRONTEND_VERSION,
