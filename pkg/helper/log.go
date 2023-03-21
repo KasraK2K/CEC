d74f8f5ee69a8830702021353c1e062b58a52edf
Kasra Karami <Kasra_K2K@yahoo.com>
2023-03-22 01:53:04 +0330

    check logger is active or not by environments

--- pkg/helper/log.go
+++ pkg/helper/log.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
+
+	"app/pkg/config"
 )
 
 type logger struct{}
@@ -13,39 +16,103 @@ var Logger logger
 /* -------------------------------------------------------------------------- */
 /*                               Stdout Loggers                               */
 /* -------------------------------------------------------------------------- */
-func (l *logger) Info(v ...interface{}) {
-	l.initLogger("\u001b[32;1mINFO\x1b[0m:    ", v...)
+func (l *logger) Debug(v ...interface{}) {
+	if strings.Contains(config.AppConfig.STDOUT_LOGS, "debug") {
+		l.initLogger("\u001b[34mDebug\u001b[0m:     ", v...)
+	}
 }
 
-func (l *logger) Verbose(v ...interface{}) {
-	l.initLogger("\u001b[34;1mVERBOSE\x1b[0m: ", v...)
+func (l *logger) Info(v ...interface{}) {
+	if strings.Contains(config.AppConfig.STDOUT_LOGS, "info") {
+		l.initLogger("\u001b[32mINFO\u001b[0m:      ", v...)
+	}
+}
+
+func (l *logger) Notice(v ...interface{}) {
+	if strings.Contains(config.AppConfig.STDOUT_LOGS, "notice") {
+		l.initLogger("\u001b[36mNOTICE\u001b[0m:    ", v...)
+	}
 }
 
 func (l *logger) Warning(v ...interface{}) {
-	l.initLogger("\u001b[38;5;226mWARNING\x1b[0m: ", v...)
+	if strings.Contains(config.AppConfig.STDOUT_LOGS, "warning") {
+		l.initLogger("\u001b[33mWARNING\u001b[0m:   ", v...)
+	}
 }
 
 func (l *logger) Error(v ...interface{}) {
-	l.initLogger("\x1b[31mERROR\x1b[0m:   ", v...)
+	if strings.Contains(config.AppConfig.STDOUT_LOGS, "error") {
+		l.initLogger("\u001b[31mERROR\u001b[0m:     ", v...)
+	}
+}
+
+func (l *logger) Critical(v ...interface{}) {
+	if strings.Contains(config.AppConfig.STDOUT_LOGS, "critical") {
+		l.initLogger("\u001b[35mCRITICAL\u001b[0m:  ", v...)
+	}
+}
+
+func (l *logger) Alert(v ...interface{}) {
+	if strings.Contains(config.AppConfig.STDOUT_LOGS, "alert") {
+		l.initLogger("\u001b[37;41mALERT\u001b[0m:     ", v...)
+	}
+}
+
+func (l *logger) Emergency(v ...interface{}) {
+	if strings.Contains(config.AppConfig.STDOUT_LOGS, "emergency") {
+		l.initLogger("\u001b[31;1mEMERGENCY\u001b[0m: ", v...)
+	}
 }
 
 /* -------------------------------------------------------------------------- */
 /*                                File Loggers                                */
 /* -------------------------------------------------------------------------- */
-func (l *logger) InfoFile(v ...interface{}) {
-	l.initFileLogger("info", v...)
+func (l *logger) DebugFile(v ...interface{}) {
+	if strings.Contains(config.AppConfig.FILE_LOGS, "debug") {
+		l.initFileLogger("debug", v...)
+	}
 }
 
-func (l *logger) VerboseFile(v ...interface{}) {
-	l.initFileLogger("verbose", v...)
+func (l *logger) InfoFile(v ...interface{}) {
+	if strings.Contains(config.AppConfig.FILE_LOGS, "info") {
+		l.initFileLogger("info", v...)
+	}
+}
+
+func (l *logger) NoticeFile(v ...interface{}) {
+	if strings.Contains(config.AppConfig.FILE_LOGS, "notice") {
+		l.initFileLogger("notice", v...)
+	}
 }
 
 func (l *logger) WarningFile(v ...interface{}) {
-	l.initFileLogger("warning", v...)
+	if strings.Contains(config.AppConfig.FILE_LOGS, "warning") {
+		l.initFileLogger("warning", v...)
+	}
 }
 
 func (l *logger) ErrorFile(v ...interface{}) {
-	l.initFileLogger("error", v...)
+	if strings.Contains(config.AppConfig.FILE_LOGS, "error") {
+		l.initFileLogger("error", v...)
+	}
+}
+
+func (l *logger) CriticalFile(v ...interface{}) {
+	if strings.Contains(config.AppConfig.FILE_LOGS, "critical") {
+		l.initFileLogger("critical", v...)
+	}
+}
+
+func (l *logger) AlertFile(v ...interface{}) {
+	if strings.Contains(config.AppConfig.FILE_LOGS, "alert") {
+		l.initFileLogger("alert", v...)
+	}
+}
+
+func (l *logger) EmergencyFile(v ...interface{}) {
+	if strings.Contains(config.AppConfig.FILE_LOGS, "emergency") {
+		l.initFileLogger("emergency", v...)
+	}
 }
 
 /* -------------------------------------------------------------------------- */
