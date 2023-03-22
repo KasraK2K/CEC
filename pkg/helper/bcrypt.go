4593a539d0c8b3d223bf3a58385ead2e99099ed2
Kasra Karami <Kasra_K2K@yahoo.com>
2023-03-22 15:45:51 +0330

    change bcrypt salt number

--- pkg/helper/bcrypt.go
+++ pkg/helper/bcrypt.go
@@ -3,7 +3,7 @@ package helper
 import "golang.org/x/crypto/bcrypt"
 
 func HashPassword(password string) (string, error) {
-	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), 7)
 	if err != nil {
 		return "", err
 	}
