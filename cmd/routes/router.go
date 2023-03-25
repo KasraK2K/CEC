323633e79ce017fa98414ca5857804ffa03fc6c2
Kasra Karami <Kasra_K2K@yahoo.com>
2023-03-25 20:23:29 +0330

    handle not found routes #1
    Co-Authored-By: kasrakaramikk <122204823+kasrakaramikk@users.noreply.github.com>

--- cmd/routes/router.go
+++ cmd/routes/router.go
@@ -1,10 +1,13 @@
 package routes
 
 import (
+	"net/http"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/monitor"
 
 	"app/cmd/modules/portal_user"
+	"app/pkg/helper"
 )
 
 func Routes(app *fiber.App) {
@@ -13,6 +16,11 @@ func Routes(app *fiber.App) {
 
 	v1 := app.Group("/v1")
 	portal_user.Routes(v1)
+
+	// Handle other routes
+	app.Use("*", func(c *fiber.Ctx) error {
+		return helper.JSON(c, "this route is not exist", http.StatusNotFound)
+	})
 }
 
 func health(c *fiber.Ctx) error {
