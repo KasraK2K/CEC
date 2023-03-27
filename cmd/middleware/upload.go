bbd1eaf07e2aac7b03b0a4ee318f3766076fa4df
Kasra Karami <Kasra_K2K@yahoo.com>
2023-03-27 12:13:11 +0330

    modify upload middleware

--- cmd/middleware/upload.go
+++ cmd/middleware/upload.go
@@ -1,7 +1,6 @@
 package middleware
 
 import (
-	"fmt"
 	"io"
 	"net/http"
 	"os"
@@ -15,7 +14,6 @@ import (
 
 func HandleMultipart(c *fiber.Ctx) error {
 	contentType := c.Get("Content-Type")
-	fmt.Println(contentType)
 	if strings.Contains(contentType, "multipart/form-data") {
 		err := upload(c)
 		if err != nil {
@@ -44,11 +42,11 @@ func upload(c *fiber.Ctx) error {
 	}
 
 	// Loop through all the files in the form data
-	for _, fileHeader := range form.File {
+	for fileFieldName, fileHeader := range form.File {
 		// Loop through all the files for this field name
 		for _, file := range fileHeader {
-			fileExt := filepath.Ext(file.Filename)
-			fileName := file.Filename[0 : len(file.Filename)-len(fileExt)]
+			// fileExt := filepath.Ext(file.Filename)
+			// originalFileName := file.Filename[0 : len(file.Filename)-len(fileExt)]
 
 			// Open the file
 			fileContent, err := file.Open()
@@ -58,7 +56,7 @@ func upload(c *fiber.Ctx) error {
 			defer fileContent.Close()
 
 			// Create a new file on the server
-			newFileName := filepath.Join(dirPath, fileName+".jpg")
+			newFileName := filepath.Join(dirPath, fileFieldName+".jpg")
 			newFile, err := os.Create(newFileName)
 			if err != nil {
 				return err
