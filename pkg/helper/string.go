626d6dc9f6f2d3d05388aa6329b1720fb1cd2fd3
Kasra Karami <Kasra_K2K@yahoo.com>
2023-03-29 15:59:53 +0330

    create string helper

--- pkg/helper/string.go
+++ pkg/helper/string.go
@@ -5,6 +5,16 @@ import (
 	"time"
 )
 
+func ReverseString(str string) string {
+	var revStr string
+
+	for _, s := range str {
+		revStr = string(s) + revStr
+	}
+
+	return revStr
+}
+
 func RandomString(length int) string {
 	randSource := rand.New(rand.NewSource(time.Now().UnixNano()))
 	charset := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
