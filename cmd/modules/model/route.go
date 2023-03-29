8f39684f18f327639b0632921fc13a380a8df995
Kasra Karami <Kasra_K2K@yahoo.com>
2023-03-29 16:05:16 +0330

    check permission in model module

--- cmd/modules/model/route.go
+++ cmd/modules/model/route.go
@@ -3,17 +3,24 @@ package model
 import (
 	"github.com/gofiber/fiber/v2"
 
+	"app/cmd/middleware"
 	"app/pkg/storage/pg"
 )
 
 func Routes(router fiber.Router) {
 	pg.Conn.Migrate(&Model{})
 
-	userGroup := router.Group("/model")
+	modelGroup := router.Group("/model")
 
-	userGroup.Post("/find", Handler.List)
-	userGroup.Post("/create", Handler.Insert)
-	userGroup.Patch("/update", Handler.Update)
-	userGroup.Delete("/archive", Handler.Archive)
-	userGroup.Patch("/restore", Handler.Restore)
+	// Needed Permissions
+	modelGroup.Use([]string{"/find", "/create"}, func(c *fiber.Ctx) error {
+		return middleware.CheckPermission(c, 2)
+	})
+
+	// Routes
+	modelGroup.Post("/find", Handler.List)
+	modelGroup.Post("/create", Handler.Insert)
+	modelGroup.Patch("/update", Handler.Update)
+	modelGroup.Delete("/archive", Handler.Archive)
+	modelGroup.Patch("/restore", Handler.Restore)
 }
