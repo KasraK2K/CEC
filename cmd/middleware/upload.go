b4533d5f28c670118f0d21eb0a8ddcec3190839f
Kasra Karami <Kasra_K2K@yahoo.com>
2023-03-30 12:21:32 +0330

    add size/ext validation for upload

--- cmd/middleware/upload.go
+++ cmd/middleware/upload.go
@@ -1,6 +1,7 @@
 package middleware
 
 import (
+	"errors"
 	"io"
 	"net/http"
 	"os"
@@ -9,6 +10,8 @@ import (
 
 	"github.com/gofiber/fiber/v2"
 
+	"app/cmd/common"
+	"app/pkg/config"
 	"app/pkg/helper"
 )
 
@@ -16,20 +19,20 @@ import (
 func HandleMultipart(c *fiber.Ctx) error {
 	contentType := c.Get("Content-Type")
 	if strings.Contains(contentType, "multipart/form-data") {
-		err := upload(c)
+		status, err := upload(c)
 		if err != nil {
-			helper.JSON(c, err.Error(), http.StatusInternalServerError)
+			return helper.JSON(c, err.Error(), status)
 		}
 		return c.Next()
 	}
 	return c.Next()
 }
 
-func upload(c *fiber.Ctx) error {
+func upload(c *fiber.Ctx) (common.Status, error) {
 	// Parse the multipart form data
 	form, err := c.MultipartForm()
 	if err != nil {
-		return err
+		return http.StatusInternalServerError, err
 	}
 
 	// Get the field name for the files
@@ -39,37 +42,55 @@ func upload(c *fiber.Ctx) error {
 	// Create a new directory based on the field name
 	dirPath := filepath.Join("uploads", mainFolder, subFolder)
 	if err := os.MkdirAll(dirPath, 0755); err != nil {
-		return err
+		return http.StatusInternalServerError, err
 	}
 
 	// Loop through all the files in the form data
 	for fileFieldName, fileHeader := range form.File {
 		// Loop through all the files for this field name
 		for _, file := range fileHeader {
-			// fileExt := filepath.Ext(file.Filename)
-			// originalFileName := file.Filename[0 : len(file.Filename)-len(fileExt)]
+			fileExt := filepath.Ext(file.Filename)
+			if fileExt == ".jpeg" {
+				fileExt = ".jpg"
+			}
+
+			// Check file type
+			isExtValid := false
+			for _, ext := range config.AppConfig.FILE_EXTENSIONS {
+				if ext == fileExt {
+					isExtValid = true
+				}
+			}
+			if !isExtValid {
+				return http.StatusNotAcceptable, errors.New("file extension is not valid")
+			}
+
+			// Check File Size
+			if file.Size > config.AppConfig.FILE_SIZE {
+				return http.StatusNotAcceptable, errors.New("file is too large")
+			}
 
 			// Open the file
 			fileContent, err := file.Open()
 			if err != nil {
-				return err
+				return http.StatusInternalServerError, err
 			}
 			defer fileContent.Close()
 
 			// Create a new file on the server
-			newFileName := filepath.Join(dirPath, fileFieldName+".jpg")
+			newFileName := filepath.Join(dirPath, fileFieldName+fileExt)
 			newFile, err := os.Create(newFileName)
 			if err != nil {
-				return err
+				return http.StatusInternalServerError, err
 			}
 			defer newFile.Close()
 
 			// Write the file to the server
 			if _, err := io.Copy(newFile, fileContent); err != nil {
-				return err
+				return http.StatusInternalServerError, err
 			}
 		}
 	}
 
-	return nil
+	return http.StatusOK, nil
 }
